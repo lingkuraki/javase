2f9f0b214203078a869197190d4828b7dedac490
kuraki <kuraki@163.com>
2019-03-13 10:43:59 +0800

    Update NewFixed.java

--- threadpool/src/main/java/com/kuraki/NewFixed.java
+++ threadpool/src/main/java/com/kuraki/NewFixed.java
@@ -1,7 +1,18 @@
 package com.kuraki;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * newFixedThreadPool 固定线程池/有界的线程池
  */
 public class NewFixed {
+
+    public static ExecutorService newFixedThreadPool(int nThreads) {
+        return new ThreadPoolExecutor(nThreads, nThreads,
+                0L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>());
+    }
 }
